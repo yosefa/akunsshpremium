dc22f5b18a58033fa7ac1ef7ce5a4d82bfbcb0f0
gbmob.tk@gmail.com <gbmob.tk@gmail.com@e10ffd28-a369-596c-23b2-4c5e95d196f8>
2014-08-25 00:54:03 +0000

    jya

--- branches/auto.sh
+++ branches/auto.sh
@@ -3,6 +3,11 @@ wget https://akunsshpremium.googlecode.com/svn/branches/crot
 wget https://akunsshpremium.googlecode.com/svn/branches/hapus
 wget https://akunsshpremium.googlecode.com/svn/branches/lagi
 wget https://akunsshpremium.googlecode.com/svn/branches/tambah
+mv add /usr/bin/add
+mv crot /usr/bin/crot
+mv hapus /usr/bin/hapus
+mv lagi /usr/bin/lagi
+mv tambah /usr/bin/tambah
 chmod +x /usr/bin/add
 chmod +x /usr/bin/crot
 chmod +x /usr/bin/hapus
