0be4f410a6e7dc585adf03750cd8ba88f80e8605
gbmob.tk@gmail.com <gbmob.tk@gmail.com@e10ffd28-a369-596c-23b2-4c5e95d196f8>
2014-08-25 00:56:26 +0000

    berhasil yes oye

--- branches/auto.sh
+++ branches/auto.sh
@@ -3,13 +3,8 @@ wget https://akunsshpremium.googlecode.com/svn/branches/crot
 wget https://akunsshpremium.googlecode.com/svn/branches/hapus
 wget https://akunsshpremium.googlecode.com/svn/branches/lagi
 wget https://akunsshpremium.googlecode.com/svn/branches/tambah
-mv add /usr/bin/add
-mv crot /usr/bin/crot
-mv hapus /usr/bin/hapus
-mv lagi /usr/bin/lagi
-mv tambah /usr/bin/tambah
-chmod +x /usr/bin/add
-chmod +x /usr/bin/crot
-chmod +x /usr/bin/hapus
-chmod +x /usr/bin/lagi
-chmod +x /usr/bin/tambah
+chmod +x add
+chmod +x crot
+chmod +x hapus
+chmod +x lagi
+chmod +x tambah
