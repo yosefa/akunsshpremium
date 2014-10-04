d33db8f09aa8a9d1e5964c6b8bd79a6f432e14ca
gbmob.tk@gmail.com <gbmob.tk@gmail.com@e10ffd28-a369-596c-23b2-4c5e95d196f8>
2014-10-04 01:54:45 +0000

    + trial

--- branches/auto.sh
+++ branches/auto.sh
@@ -3,8 +3,10 @@ wget https://akunsshpremium.googlecode.com/svn/branches/crot
 wget https://akunsshpremium.googlecode.com/svn/branches/hapus
 wget https://akunsshpremium.googlecode.com/svn/branches/lagi
 wget https://akunsshpremium.googlecode.com/svn/branches/tambah
+wget https://akunsshpremium.googlecode.com/svn/branches/trial
 chmod +x add
 chmod +x crot
 chmod +x hapus
 chmod +x lagi
 chmod +x tambah
+chmod +x trial
