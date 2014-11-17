bff3da7e416a37e9be97ec93509cb14b91f3c442
gbmob.tk@gmail.com <gbmob.tk@gmail.com@e10ffd28-a369-596c-23b2-4c5e95d196f8>
2014-11-17 03:31:50 +0000

    --no-check-certificate

--- branches/autokvm.sh
+++ branches/autokvm.sh
@@ -3,8 +3,26 @@ wget https://akunsshpremium.googlecode.com/svn/branches/crot --no-check-certific
 wget https://akunsshpremium.googlecode.com/svn/branches/hapus --no-check-certificate
 wget https://akunsshpremium.googlecode.com/svn/branches/lagi --no-check-certificate
 wget https://akunsshpremium.googlecode.com/svn/branches/tambah --no-check-certificate
+wget https://akunsshpremium.googlecode.com/svn/branches/trial --no-check-certificate
+wget https://akunsshpremium.googlecode.com/svn/branches/masuk --no-check-certificate
+wget https://akunsshpremium.googlecode.com/svn/branches/expuser --no-check-certificate
+wget https://akunsshpremium.googlecode.com/svn/branches/un.sh --no-check-certificate
 chmod +x add
 chmod +x crot
 chmod +x hapus
 chmod +x lagi
 chmod +x tambah
+chmod +x trial
+chmod +x masuk
+chmod +x expuser
+chmod +x un.sh
+apt-get install dos2unix
+dos2unix add
+dos2unix crot
+dos2unix hapus
+dos2unix lagi
+dos2unix tambah
+dos2unix trial
+dos2unix masuk
+dos2unix expuser
+dos2unix un.sh
