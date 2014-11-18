063511edf2a6d42c21a8a6997852caa53a7a1c6d
gbmob.tk@gmail.com <gbmob.tk@gmail.com@e10ffd28-a369-596c-23b2-4c5e95d196f8>
2014-11-18 09:32:07 +0000

    kurang

--- branches/auto.sh
+++ branches/auto.sh
@@ -3,6 +3,7 @@ wget https://akunsshpremium.googlecode.com/svn/branches/crot
 wget https://akunsshpremium.googlecode.com/svn/branches/hapus
 wget https://akunsshpremium.googlecode.com/svn/branches/lagi
 wget https://akunsshpremium.googlecode.com/svn/branches/tambah
+wget https://akunsshpremium.googlecode.com/svn/branches/kurang
 wget https://akunsshpremium.googlecode.com/svn/branches/trial
 wget https://akunsshpremium.googlecode.com/svn/branches/masuk
 wget https://akunsshpremium.googlecode.com/svn/branches/expuser
@@ -12,6 +13,7 @@ chmod +x crot
 chmod +x hapus
 chmod +x lagi
 chmod +x tambah
+chmod +x kurang
 chmod +x trial
 chmod +x masuk
 chmod +x expuser
@@ -22,6 +24,7 @@ dos2unix crot
 dos2unix hapus
 dos2unix lagi
 dos2unix tambah
+dos2unix kurang
 dos2unix trial
 dos2unix masuk
 dos2unix expuser
